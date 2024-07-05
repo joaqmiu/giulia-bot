f59cc2f46b8a7f598d5de99a869c33c6a872f44b
Joaquim <joaqmiu@outlook.com.br>
2024-07-05 14:40:04 -0300

    Update render.sh

--- render.sh
+++ render.sh
@@ -2,10 +2,13 @@
 
 # método para render 
 
-wget https://download1075.mediafire.com/u9laza5wyskgznXM8S9apruxd_gd0W0GDWnlKJXD33dePMkF3jDqqQczIOsXgpBvROVI6tmoS5zweZC9YOm2COcZwaAAKhjBiU81rGBo-1gwBmCOv98t-sljc3UYZMOxZVoSD9__7-c-W4iHPSA_0YvEzigs71iZKfkR3Kz_LSkL57w/g2kjei1tgxpuy41/servidor%282%29.zip
-unzip "servidor(2).zip"
-rm "servidor(2).zip"
+wget https://download1532.mediafire.com/oevcu5n542xgrorbKMlsYfKFtB0xYl4jwhhNAPK9qfwJfbsx126HeGcobmXR2FjpddeZNYm-Xc9Y3UOAjSqmrMygxJSUem5RqH_TQXsw4yQgNEisomjqY9MITrvNPfQgIuhiJsRZX_V1JF9qfoM3YxUDR5Wl8YzXu_uR26Zv5cp9xus/f8dzzh6ebje8vef/serverender.zip
+
+unzip serverender.zip
+rm serverender.zip
+
 npm install
 npm install ws express
 npm install pm2 -g
+
 node app.js
