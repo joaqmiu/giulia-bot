976495a14eb351a8d9e7d22e1e39ab2cbba70507
Joaquim <joaqmiu@outlook.com.br>
2024-07-08 07:52:42 -0300

    Update bot.js

--- aut/bot.js
+++ aut/bot.js
@@ -1,4 +1,4 @@
-const { default: makeWASocket, useMultiFileAuthState } = require('@whiskeysockets/baileys');
+const { default: makeWASocket, useMultiFileAuthState, delay } = require('@whiskeysockets/baileys');
 const Pino = require('pino');
 const { handleMessage } = require('../msgs');
 
@@ -11,18 +11,44 @@ const startBot = async () => {
     });
 
     sock.ev.on('creds.update', saveCreds);
+
     sock.ev.on('messages.upsert', async ({ messages }) => {
         const msg = messages[0];
         if (msg.key.fromMe) return;
+
+        await sock.readMessages([{
+            remoteJid: msg.key.remoteJid,
+            id: msg.key.id,
+            participant: msg.key.participant
+        }]);
+
         await handleMessage(sock, msg);
     });
 
-    sock.ev.on('connection.update', (update) => {
-        const { connection, lastDisconnect, qr } = update;
+    sock.ev.on('groups.update', async (groups) => {
+        console.log('Grupos atualizados', groups);
+    });
+
+    sock.ev.on('chats.update', async (chats) => {
+        console.log('Chats atualizados', chats);
+    });
+
+    sock.ev.on('connection.update', async (update) => {
+        const { connection, lastDisconnect } = update;
         if (connection === 'close') {
             startBot();
         } else if (connection === 'open') {
             console.log('Conectado!');
+
+            const chats = await sock.store.chats.all();
+            for (const chat of chats) {
+                await sock.readMessages([{
+                    remoteJid: chat.id,
+                    id: chat.messages[0]?.key.id,
+                    participant: chat.messages[0]?.key.participant
+                }]);
+                await delay(100);
+            }
         }
     });
 };
